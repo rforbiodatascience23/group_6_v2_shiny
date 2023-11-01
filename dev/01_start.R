e1086f1ff75c48a11ed0904ee5ea27c95cb74d22
JavierLopezBort <s223237@student.dtu.dk>
2023-11-01 16:09:56 +0000

    changes

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,11 +20,11 @@
 ##
 golem::fill_desc(
   pkg_name = "CentralDogmaMBApp", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "CentralDogmaMBApp", # The Title of the package containing the App
+  pkg_description = "PackageDescription", # The Description of the package containing the App
+  author_first_name = "Javier", # Your First Name
+  author_last_name = "Lopez-Bort", # Your Last Name
+  author_email = "s223237@student.dtu.dk", # Your Email
   repo_url = NULL, # The URL of the GitHub Repo (optional),
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
